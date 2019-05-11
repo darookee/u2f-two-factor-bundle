addd194eb9625b1d90ab08675df590474ba10ba3
stephanvierkant <stephan@vierkant.org>
2019-05-11 14:29:11 +0200

    return type for controller action (and removed useless docblock)

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -6,16 +6,14 @@ use R\U2FTwoFactorBundle\Event\RegisterEvent;
 use R\U2FTwoFactorBundle\Security\TwoFactor\Provider\U2F\U2FAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Nils Uliczka
  */
 class RegisterController extends AbstractController
 {
-    /**
-     * @param Request $request
-     **/
-    public function u2fAction(Request $request)
+    public function u2fAction(Request $request) : Response
     {
         $u2fAuthenticator = $this->get(U2FAuthenticator::class);
         if ($request->isMethod('POST')) {
